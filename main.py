d98cdb07d12a207cdbdfb271ce068b2fb8688f30
irule4567 <33972693+irule4567@users.noreply.github.com>
2023-09-23 14:37:54 -0400

    Main keyword comparison complete
    Added main functionality for words in the script to be compared with keywords in locations. Both the script and list of locations are currently both set to basic test parameters.

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ class Location:
     def __init__(self, name):
         self.name = name
         self.keywords = []
+        self.keywordValue = 0
     def add_keyword(self, keyword):
         self.keywords.append(keyword)
     def get_name(self):
         return self.name
     def get_keywords(self):
         return self.keywords
+    def get_value(self):
+        return self.keywordValue
+    def add_value(self):
+        self.keywordValue = self.keywordValue + 1
 
+# Reads in the script and stores each word
 def getScript():
     #Reads in script
     script = open("Test script.txt")
@@ -21,6 +27,8 @@ def getScript():
         if scriptChars[char] == "\n" or scriptChars[char] == "\t" or scriptChars[char] == " ":
             scriptwords.append(tempWord)
             tempWord = ""
+        elif scriptChars[char] == "":
+            tempWord = tempWord + scriptChars[char]
         else:
             tempWord = tempWord + scriptChars[char]
     return scriptwords
@@ -28,6 +36,7 @@ def getScript():
     #for word in range(len(keywords)):
     #    print(keywords[word])
 
+# Reads in locations and their keywords
 def getLocations():
     locationstxt = open("Test location+keywords.txt")
     locations = []
@@ -43,6 +52,8 @@ def getLocations():
         elif locationChars[char] == "," or locationChars[char] == "\n":
             locations[locationPos].add_keyword(tempWord)
             tempWord = ""
+        elif locationChars[char] == "":
+            tempWord = tempWord + locationChars[char]
         else:
             tempWord = tempWord + locationChars[char]
     return locations
@@ -52,10 +63,24 @@ def getLocations():
     #    for keyword in range(len(locations[location].get_keywords())):
     #        print(locations[location].get_keywords()[keyword])
 
-
+# Takes in the list of script words and list of locations and compares the words to the keywords to find which location matches more
+def find_ideal_locations(script, location):
+    for word in range(len(script)):
+        for place in range(len(location)):
+            for keyword in range(len(location[place].get_keywords())):
+                if script[word] == location[place].get_keywords()[keyword]:
+                    location[place].add_value()
+    bestloc = ""
+    topscore = 0
+    for place in range(len(location)):
+        if location[place].get_value() >= topscore:
+            topscore = location[place].get_value()
+            bestloc = location[place].get_name()
+    print(bestloc)
 
 
 
 if __name__ == "__main__":
     wordsinscript = getScript()
     locationlist = getLocations()
+    find_ideal_locations(wordsinscript, locationlist)
